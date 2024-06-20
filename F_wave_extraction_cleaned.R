c852ee9ad1b11e556a93fa5921dec6e1ffcf4fd9
ssandu2 <140130590+ssandu2@users.noreply.github.com>
2024-06-19 23:37:58 -0500

    Update F_wave_extraction_cleaned.R

--- F_wave_extraction_cleaned.R
+++ F_wave_extraction_cleaned.R
@@ -13,7 +13,6 @@ sample_info <-
   read.csv('C:/Users/shsan/Documents/Medical School/M2 Year/Darbar 2023-2024/PhysioNet Projects/lobachevsky-university-electrocardiography-database-1.0.1/ludb.csv')
 View(sample_info)
 
-
 #Select records in AF
 rec_dir <- fs::path("C:/Users/shsan/Documents/Medical School/M2 Year/Darbar 2023-2024/PhysioNet Projects/lobachevsky-university-electrocardiography-database-1.0.1/data")
 af_samples_index <- ifelse(grepl("Atrial fibrillation", sample_info$Rhythms), sample_info$ID,0)
@@ -27,6 +26,12 @@ test <- read_wfdb(record = af_samples_index[2],
                  record_dir = rec_dir,
                  annotator = 'i')
 ggm(test)
+
+# Read records from MIT BIH database (test)
+#test_1 <- read_wfdb(record = 1001, record_dir = rec_dir, annotation = atr) #read file
+#View(test_1$sample)
+
+
 # Read records in loop.*Taken from LUDB_input_prep*
 ann = 'i'
 for (rec in 1:length(af_samples_index)) {
@@ -87,7 +92,7 @@ for (i in 1:dim(samples)[[1]]){
 }
 
 #Graph with color coded regions
-test_sample <- 1 #test_sample refers to index of sample, not record number( e.g., test sample <-1 is record 8)
+test_sample <- 2 #test_sample refers to index of sample, not record number( e.g., test sample <-1 is record 8)
 test_frame <- data.frame(Time = 1:5000 / 500, Signal = samples[test_sample, , 1])
 #color code
 colors <- samples[test_sample, , 2]
@@ -114,26 +119,43 @@ test_plot2 <- ggplot(test_frame2, aes(Time, fwave_samplevalues)) +
 
 test_plot2
 
-#ECG Fourier transform
-ecg_fft<- abs(fft(samples[test_sample, ,1]))
+#ECG Fourier transform -> Power spectrum. Apply Re() to fft(samples[test_sample, ,1])
+# for power spectrum
+ecg_fft<- fft(samples[test_sample, ,1])
+plot(ecg_fft)
 samp_freq <- 500
-freq <- seq(0, samp_freq/2, length = length(ecg_fft)/2)
-fft_dataframe<- data.frame(freq, ecg_fft[1:(length(ecg_fft)/2)])
-fft_plot <- ggplot(fft_dataframe, aes(freq, ecg_fft[1:(length(ecg_fft)/2)])) + geom_path(linewidth =
-                                                                      1, aes(group = 1)) + geom_point() 
+
+n <- length(samples[test_sample, ,1])
+power <- (2* (ecg_fft)^2) / n^2
+freq <- (0:(n-1)) * (samp_freq/n)
+freq <- freq[1:n/2]
+power <- power[1:n/2]
+power_spec_df <- data.frame(frequency = freq, Power <- power )
+
+power_spec_ggplot <- ggplot(power_spec_df, aes(freq, power)) + 
+  geom_line() + xlim(0,50) + 
+  labs(title = "Power spectrum of ECG",x = "Frequency (Hz)", y = "Power") + theme_minimal()
+
 #F wave extraction
-f_wave_indices <- which(freq > 4 & freq < 10)
-ecg_fft_new <- ecg_fft
+#f_wave_indices <- which(freq > 4 & freq < 10)
+ecg_fft_new <- ecg_fft[1:(n/2 +1)]
+f_wave_indices <- which(freq >= 4 & freq <= 10)
 ecg_fft_new[-f_wave_indices] <- 0
 
 #Reconstruct ECG using new FFT
-ecg_fwave_only <- Re(fft(ecg_fft_new, inverse = TRUE)) / length(ecg_fft_new)
-fft_frame <- data.frame(Time2 = 1:5000/500, Signal2 = ecg_fwave_only)
+full_fft <- c(ecg_fft_new, rev(Conj(ecg_fft_new[-1])))
+length(full_fft)
+full_fft <- full_fft[1:n]
+reecg_fwave_only <- Re(fft(full_fft, inverse = TRUE)) / n
+re_ecg_frame <- data.frame(Time2 = 1:5000/500, Signal2 = reecg_fwave_only)
+re_ecgfwaveggplot <- ggplot(re_ecg_frame, aes(Time2, Signal2)) + 
+  geom_line()  + 
+  labs(title = "Reconstructed ECG f waves",x = "Time", y = "Amplitude (mV)") + theme_minimal()
 
 #Graph reconstructed ECG using new FFT.
 par(mfrow = c(2,1))
 plot(samples[test_sample, ,1], type = "l", main = "Original ECG Signal", xlab = "Time", ylab = "Amplitude")
-plot(ecg_fwave_only, type = "l", main = "Reconstructed ECG Signal", xlab = "Time", ylab = "Amplitude")
+plot(reecg_fwave_only, type = "l", main = "Reconstructed ECG Signal", xlab = "Time", ylab = "Amplitude")
 
 #PCA--------------------------------------------------------------------
 
